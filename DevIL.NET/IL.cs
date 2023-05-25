b7dc9e4424c09b8b38c420b8c88e8da5a76b9dba
Tyler Befferman <TylerBeff@gmail.com>
2023-05-24 19:00:58 -0600

    Initial commit

---
+++ DevIL.NET/IL.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DevIL.NET
+{
+	public static class IL
+	{
+		private const string dllName = "DevIL.dll";
+
+		[DllImport(dllName, EntryPoint = "ilActiveFace")]
+		public static extern bool ActiveFace(int Number);
+
+		[DllImport(dllName, EntryPoint = "ilActiveImage")]
+		public static extern bool ActiveImage(int Number);
+
+		[DllImport(dllName, EntryPoint = "ilActiveLayer")]
+		public static extern bool ActiveLayer(int Number);
+
+		[DllImport(dllName, EntryPoint = "ilActiveMipmap")]
+		public static extern bool ActiveMipmap(int Number);
+
+		[DllImport(dllName, EntryPoint = "ilApplyPal")]
+		public static extern bool ApplyPal(string FileName);
+
+		[DllImport(dllName, EntryPoint = "ilApplyProfile")]
+		public static extern bool ApplyProfile(string InProfile, string OutProfile);
+
+		[DllImport(dllName, EntryPoint = "ilBindImage")]
+		public static extern void BindImage(int Image);
+
+		[DllImport(dllName, EntryPoint = "ilBlit")]
+		public static extern bool Blit(int Source, int DestX, int DestY, int DestZ, int SrcX, int SrcY, int SrcZ, int Width, int Height, int Depth);
+
+		[DllImport(dllName, EntryPoint = "ilClampNTSC")]
+		public static extern bool ClampNTSC();
+
+		[DllImport(dllName, EntryPoint = "ilClearColour")]
+		public static extern void ClearColour(float Red, float Green, float Blue, float Alpha);
+
+		[DllImport(dllName, EntryPoint = "ilClearImage")]
+		public static extern bool ClearImage();
+
+		[DllImport(dllName, EntryPoint = "ilCloneCurImage")]
+		public static extern int CloneCurImage();
+
+		[DllImport(dllName, EntryPoint = "ilCompressDXT")]
+		public static extern IntPtr CompressDXT(IntPtr Data, int Width, int Height, int Depth, CompressedDataFormat DXTCFormat, ref int DXTCSize);
+
+		[DllImport(dllName, EntryPoint = "ilCompressFunc")]
+		public static extern bool CompressFunc(CompressMode Mode);
+
+		[DllImport(dllName, EntryPoint = "ilConvertImage")]
+		public static extern bool ConvertImage(ChannelFormat DestFormat, ChannelType DestType);
+
+		[DllImport(dllName, EntryPoint = "ilConvertPal")]
+		public static extern bool ConvertPal(PaletteType DestFormat);
+
+		[DllImport(dllName, EntryPoint = "ilCopyImage")]
+		public static extern bool CopyImage(int Src);
+
+		[DllImport(dllName, EntryPoint = "ilCopyPixels")]
+		public static extern int CopyPixels(int XOff, int YOff, int ZOff, int Width, int Height, int Depth, ChannelFormat Format, ChannelType Type, byte[] Data);
+
+		[DllImport(dllName, EntryPoint = "ilCreateSubImage")]
+		public static extern int CreateSubImage(SubImageType Type, int Num);
+
+		[DllImport(dllName, EntryPoint = "ilDefaultImage")]
+		public static extern bool DefaultImage();
+
+		[DllImport(dllName, EntryPoint = "ilDeleteImage")]
+		public static extern void DeleteImage(int Num);
+
+		[DllImport(dllName, EntryPoint = "ilDeleteImages")]
+		public static extern void DeleteImages(IntPtr Num, int[] Images);
+
+		[DllImport(dllName, EntryPoint = "ilDetermineType")]
+		public static extern ImageType DetermineType(string FileName);
+
+		[DllImport(dllName, EntryPoint = "ilDetermineTypeF")]
+		public static extern ImageType DetermineTypeF(byte[] File);
+
+		[DllImport(dllName, EntryPoint = "ilDetermineTypeL")]
+		public static extern ImageType DetermineTypeL(byte[] Lump, int Size);
+
+		[DllImport(dllName, EntryPoint = "ilDisable")]
+		public static extern bool Disable(EnableCap Mode);
+
+		[DllImport(dllName, EntryPoint = "ilDxtcDataToImage")]
+		public static extern bool DxtcDataToImage();
+
+		[DllImport(dllName, EntryPoint = "ilDxtcDataToSurface")]
+		public static extern bool DxtcDataToSurface();
+
+		[DllImport(dllName, EntryPoint = "ilEnable")]
+		public static extern bool Enable(EnableCap Mode);
+
+		[DllImport(dllName, EntryPoint = "ilFlipSurfaceDxtcData")]
+		public static extern void FlipSurfaceDxtcData();
+
+		[DllImport(dllName, EntryPoint = "ilFormatFunc")]
+		public static extern bool FormatFunc(ChannelFormat Mode);
+
+		[DllImport(dllName, EntryPoint = "ilGenImages")]
+		public static extern void GenImages(IntPtr Num, int[] Images);
+
+		[DllImport(dllName, EntryPoint = "ilGenImage")]
+		public static extern int GenImage();
+
+		[DllImport(dllName, EntryPoint = "ilGetAlpha")]
+		public static extern IntPtr GetAlpha(ChannelType Type);
+
+		[DllImport(dllName, EntryPoint = "ilGetBoolean")]
+		public static extern bool GetBoolean(BooleanName Mode);
+
+		[DllImport(dllName, EntryPoint = "ilGetBooleanv")]
+		public static extern void GetBooleanv(BooleanName Mode, bool[] Param);
+
+		[DllImport(dllName, EntryPoint = "ilGetData")]
+		public static extern IntPtr GetData();
+
+		[DllImport(dllName, EntryPoint = "ilGetDXTCData")]
+		public static extern int GetDXTCData(byte[] Buffer, int BufferSize, CompressedDataFormat DXTCFormat);
+
+		[DllImport(dllName, EntryPoint = "ilGetError")]
+		public static extern ErrorCode GetError();
+
+		[DllImport(dllName, EntryPoint = "ilGetInteger")]
+		public static extern int GetInteger(IntName Mode);
+
+		[DllImport(dllName, EntryPoint = "ilGetIntegerv")]
+		public static extern void GetIntegerv(IntName Mode, int[] Param);
+
+		[DllImport(dllName, EntryPoint = "ilGetLumpPos")]
+		public static extern int GetLumpPos();
+
+		[DllImport(dllName, EntryPoint = "ilGetPalette")]
+		public static extern IntPtr GetPalette();
+
+		[DllImport(dllName, EntryPoint = "ilGetString")]
+		public static extern string GetString(StringName StringName);
+
+		//[DllImport(lib, EntryPoint="ilHint")]
+		//public static extern void Hint(ILenum Target, ILenum Mode);
+
+		[DllImport(dllName, EntryPoint = "ilInvertSurfaceDxtcDataAlpha")]
+		public static extern bool InvertSurfaceDxtcDataAlpha();
+
+		[DllImport(dllName, EntryPoint = "ilInit")]
+		public static extern void Init();
+
+		[DllImport(dllName, EntryPoint = "ilImageToDxtcData")]
+		public static extern bool ImageToDxtcData(CompressedDataFormat Format);
+
+		[DllImport(dllName, EntryPoint = "ilIsDisabled")]
+		public static extern bool IsDisabled(EnableCap Mode);
+
+		[DllImport(dllName, EntryPoint = "ilIsEnabled")]
+		public static extern bool IsEnabled(EnableCap Mode);
+
+		[DllImport(dllName, EntryPoint = "ilIsImage")]
+		public static extern bool IsImage(int Image);
+
+		[DllImport(dllName, EntryPoint = "ilIsValid")]
+		public static extern bool IsValid(ImageType Type, string FileName);
+
+		[DllImport(dllName, EntryPoint = "ilIsValidF")]
+		public static extern bool IsValidF(ImageType Type, byte[] File);
+
+		[DllImport(dllName, EntryPoint = "ilIsValidL")]
+		public static extern bool IsValidL(ImageType Type, byte[] Lump, int Size);
+
+		[DllImport(dllName, EntryPoint = "ilKeyColour")]
+		public static extern void KeyColour(float Red, float Green, float Blue, float Alpha);
+
+		[DllImport(dllName, EntryPoint = "ilLoad")]
+		public static extern bool Load(ImageType Type, string FileName);
+
+		[DllImport(dllName, EntryPoint = "ilLoadF")]
+		public static extern bool LoadF(ImageType Type, byte[] File);
+
+		[DllImport(dllName, EntryPoint = "ilLoadImage")]
+		public static extern bool LoadImage(string FileName);
+
+		[DllImport(dllName, EntryPoint = "ilLoadL")]
+		public static extern bool LoadL(ImageType Type, byte[] Lump, int Size);
+
+		[DllImport(dllName, EntryPoint = "ilLoadPal")]
+		public static extern bool LoadPal(string FileName);
+
+		[DllImport(dllName, EntryPoint = "ilModAlpha")]
+		public static extern void ModAlpha(float AlphaValue);
+
+		[DllImport(dllName, EntryPoint = "ilOriginFunc")]
+		public static extern bool OriginFunc(OriginMode Mode);
+
+		[DllImport(dllName, EntryPoint = "ilOverlayImage")]
+		public static extern bool OverlayImage(int Source, int XCoord, int YCoord, int ZCoord);
+
+		[DllImport(dllName, EntryPoint = "ilPopAttrib")]
+		public static extern void PopAttrib();
+
+		[DllImport(dllName, EntryPoint = "ilPushAttrib")]
+		public static extern void PushAttrib(int Bits);
+
+		[DllImport(dllName, EntryPoint = "ilRegisterFormat")]
+		public static extern void RegisterFormat(ChannelFormat Format);
+
+		//[DllImport(lib, EntryPoint="ilRegisterLoad")]
+		//public static extern bool RegisterLoad(string Ext, IL_LOADPROC Load);
+
+		[DllImport(dllName, EntryPoint = "ilRegisterMipNum")]
+		public static extern bool RegisterMipNum(int Num);
+
+		[DllImport(dllName, EntryPoint = "ilRegisterNumFaces")]
+		public static extern bool RegisterNumFaces(int Num);
+
+		[DllImport(dllName, EntryPoint = "ilRegisterNumImages")]
+		public static extern bool RegisterNumImages(int Num);
+
+		[DllImport(dllName, EntryPoint = "ilRegisterOrigin")]
+		public static extern void RegisterOrigin(OriginMode Origin);
+
+		[DllImport(dllName, EntryPoint = "ilRegisterPal")]
+		public static extern void RegisterPal(byte[] Pal, int Size, PaletteType Type);
+
+		//[DllImport(lib, EntryPoint="ilRegisterSave")]
+		//public static extern bool RegisterSave(string Ext, IL_SAVEPROC Save);
+
+		[DllImport(dllName, EntryPoint = "ilRegisterType")]
+		public static extern void RegisterType(ChannelType Type);
+
+		[DllImport(dllName, EntryPoint = "ilRemoveLoad")]
+		public static extern bool RemoveLoad(string Ext);
+
+		[DllImport(dllName, EntryPoint = "ilRemoveSave")]
+		public static extern bool RemoveSave(string Ext);
+
+		[DllImport(dllName, EntryPoint = "ilResetMemory")]
+		public static extern void ResetMemory(); // Deprecated
+
+		[DllImport(dllName, EntryPoint = "ilResetRead")]
+		public static extern void ResetRead();
+
+		[DllImport(dllName, EntryPoint = "ilResetWrite")]
+		public static extern void ResetWrite();
+
+		[DllImport(dllName, EntryPoint = "ilSave")]
+		public static extern bool Save(ImageType Type, string FileName);
+
+		[DllImport(dllName, EntryPoint = "ilSaveF")]
+		public static extern int SaveF(ImageType Type, byte[] File);
+
+		[DllImport(dllName, EntryPoint = "ilSaveImage")]
+		public static extern bool SaveImage(string FileName);
+
+		[DllImport(dllName, EntryPoint = "ilSaveL")]
+		public static extern int SaveL(ImageType Type, byte[] Lump, int Size);
+
+		[DllImport(dllName, EntryPoint = "ilSavePal")]
+		public static extern bool SavePal(string FileName);
+
+		[DllImport(dllName, EntryPoint = "ilSetAlpha")]
+		public static extern bool SetAlpha(float AlphaValue);
+
+		[DllImport(dllName, EntryPoint = "ilSetData")]
+		public static extern bool SetData(byte[] Data);
+
+		[DllImport(dllName, EntryPoint = "ilSetDuration")]
+		public static extern bool SetDuration(int Duration);
+
+		[DllImport(dllName, EntryPoint = "ilSetInteger")]
+		public static extern void SetInteger(IntName Mode, int Param);
+
+		//[DllImport(lib, EntryPoint="ilSetMemory")]
+		//public static extern void SetMemory(mAlloc, mFree);
+
+		[DllImport(dllName, EntryPoint = "ilSetPixels")]
+		public static extern void SetPixels(int XOff, int YOff, int ZOff, int Width, int Height, int Depth, ChannelFormat Format, ChannelType Type, byte[] Data);
+
+		//[DllImport(lib, EntryPoint="ilSetRead")]
+		//public static extern void SetRead(fOpenRProc, fCloseRProc, fEofProc, fGetcProc, fReadProc, fSeekRProc, fTellRProc);
+
+		[DllImport(dllName, EntryPoint = "ilSetString")]
+		public static extern void SetString(StringName Mode, string String);
+
+		//[DllImport(lib, EntryPoint="ilSetWrite")]
+		//public static extern void SetWrite(fOpenWProc, fCloseWProc, fPutcProc, fSeekWProc, fTellWProc, fWriteProc);
+
+		[DllImport(dllName, EntryPoint = "ilShutDown")]
+		public static extern void ShutDown();
+
+		[DllImport(dllName, EntryPoint = "ilSurfaceToDxtcData")]
+		public static extern bool SurfaceToDxtcData(CompressedDataFormat Format);
+
+		[DllImport(dllName, EntryPoint = "ilTexImage")]
+		public static extern bool TexImage(int Width, int Height, int Depth, byte NumChannels, ChannelFormat Format, ChannelType Type, byte[] Data);
+
+		[DllImport(dllName, EntryPoint = "ilTexImageDxtc")]
+		public static extern bool TexImageDxtc(int w, int h, int d, CompressedDataFormat DxtFormat, IntPtr data);
+
+		//[DllImport(dllName, EntryPoint = "ilTexImageSurface")]
+		//public static extern bool TexImageSurface(int Width, int Height, int Depth, byte NumChannels, ChannelFormat Format, ChannelType Type, byte[] Data);
+
+		[DllImport(dllName, EntryPoint = "ilTypeFromExt")]
+		public static extern ImageType TypeFromExt(string FileName);
+
+		[DllImport(dllName, EntryPoint = "ilTypeFunc")]
+		public static extern bool TypeFunc(ChannelType Mode);
+
+		[DllImport(dllName, EntryPoint = "ilLoadData")]
+		public static extern bool LoadData(string FileName, int Width, int Height, int Depth, byte Bpp);
+
+		[DllImport(dllName, EntryPoint = "ilLoadDataF")]
+		public static extern bool LoadDataF(byte[] File, int Width, int Height, int Depth, byte Bpp);
+
+		[DllImport(dllName, EntryPoint = "ilLoadDataL")]
+		public static extern bool LoadDataL(byte[] Lump, int Size, int Width, int Height, int Depth, byte Bpp);
+
+		[DllImport(dllName, EntryPoint = "ilSaveData")]
+		public static extern bool SaveData(string FileName);
+	}
+}
---
+++ DevIL.NET/ILU.cs
@@ -0,0 +1,256 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DevIL.NET
+{
+	public static class ILU
+	{
+		private const string dllName = "ILU.dll";
+
+		public class Info
+		{
+			/// <summary> the image's id </summary>
+			public int Id;
+
+			/// <summary> the image's data </summary>
+			public IntPtr Data;
+
+			/// <summary> the image's width </summary>
+			public int Width;
+
+			/// <summary> the image's height </summary>
+			public int Height;
+
+			/// <summary> the image's depth </summary>
+			public int Depth;
+
+			/// <summary> bytes per pixel (not bits) of the image </summary>
+			public byte Bpp;
+
+			/// <summary> the total size of the data (in bytes) </summary>
+			public int SizeOfData;
+
+			/// <summary> image format (in IL enum style) </summary>
+			public ChannelFormat Format;
+
+			/// <summary> image type (in IL enum style) </summary>
+			public ChannelType Type;
+
+			/// <summary> origin of the image </summary>
+			public OriginMode Origin;
+
+			/// <summary> the image's palette </summary>
+			public IntPtr Palette;
+
+			/// <summary> palette type </summary>
+			public PaletteType PalType;
+
+			/// <summary> palette size </summary>
+			public int PalSize;
+
+			/// <summary> flags for what cube map sides are present </summary>
+			public CubeFace CubeFlags;
+
+			/// <summary> number of images following </summary>
+			public int NumNext;
+
+			/// <summary> number of mipmaps </summary>
+			public int NumMips;
+
+			/// <summary> number of layers </summary>
+			public int NumLayers;
+
+			public override string ToString()
+			{
+				var str = string.Join("; ",
+					$"Id = {Id}",
+					$"Width = {Width}",
+					$"Height = {Height}",
+					$"Depth = {Depth}",
+					$"Bpp = {Bpp}",
+					$"SizeOfData = {SizeOfData}",
+					$"Type = {Type}",
+					$"Origin = {Origin}",
+					$"PalType = {PalType}",
+					$"PalSize = {PalSize}",
+					$"CubeFlags = {CubeFlags}",
+					$"NumNext = {NumNext}",
+					$"NumMips = {NumMips}",
+					$"NumLayers = {NumLayers}"
+				);
+
+				return $"ILU.Info {str}";
+			}
+		}
+
+		public class P2f
+		{
+			public float x;
+			public float y;
+
+			public P2f(float x, float y)
+			{
+				this.x = x;
+				this.y = y;
+			}
+		}
+
+		public class P2i
+		{
+			public int x;
+			public int y;
+
+			public P2i(int x, int y)
+			{
+				this.x = x;
+				this.y = y;
+			}
+		}
+
+		[DllImport(dllName, EntryPoint = "iluAlienify")]
+		public static extern bool Alienify();
+
+		[DllImport(dllName, EntryPoint = "iluBlurAvg")]
+		public static extern bool BlurAvg(int Iter);
+
+		[DllImport(dllName, EntryPoint = "iluBlurGaussian")]
+		public static extern bool BlurGaussian(int Iter);
+
+		[DllImport(dllName, EntryPoint = "iluBuildMipmaps")]
+		public static extern bool BuildMipmaps();
+
+		[DllImport(dllName, EntryPoint = "iluColoursUsed")]
+		public static extern int ColoursUsed();
+
+		[DllImport(dllName, EntryPoint = "iluCompareImage")]
+		public static extern bool CompareImage(int Comp);
+
+		[DllImport(dllName, EntryPoint = "iluContrast")]
+		public static extern bool Contrast(float Contrast);
+
+		[DllImport(dllName, EntryPoint = "iluCrop")]
+		public static extern bool Crop(int XOff, int YOff, int ZOff, int Width, int Height, int Depth);
+
+		[DllImport(dllName, EntryPoint = "iluDeleteImage")]
+		public static extern void DeleteImage(int Id); // Deprecated
+
+		[DllImport(dllName, EntryPoint = "iluEdgeDetectE")]
+		public static extern bool EdgeDetectE();
+
+		[DllImport(dllName, EntryPoint = "iluEdgeDetectP")]
+		public static extern bool EdgeDetectP();
+
+		[DllImport(dllName, EntryPoint = "iluEdgeDetectS")]
+		public static extern bool EdgeDetectS();
+
+		[DllImport(dllName, EntryPoint = "iluEmboss")]
+		public static extern bool Emboss();
+
+		[DllImport(dllName, EntryPoint = "iluEnlargeCanvas")]
+		public static extern bool EnlargeCanvas(int Width, int Height, int Depth);
+
+		[DllImport(dllName, EntryPoint = "iluEnlargeImage")]
+		public static extern bool EnlargeImage(float XDim, float YDim, float ZDim);
+
+		[DllImport(dllName, EntryPoint = "iluEqualize")]
+		public static extern bool Equalize();
+
+		[DllImport(dllName, EntryPoint = "iluEqualize2")]
+		public static extern bool Equalize2();
+
+		[DllImport(dllName, EntryPoint = "iluErrorString")]
+		public static extern IntPtr ErrorString(ErrorCode Error);
+
+		[DllImport(dllName, EntryPoint = "iluConvolution")]
+		public static extern bool Convolution(int[] matrix, int scale, int bias);
+
+		[DllImport(dllName, EntryPoint = "iluFlipImage")]
+		public static extern bool FlipImage();
+
+		[DllImport(dllName, EntryPoint = "iluGammaCorrect")]
+		public static extern bool GammaCorrect(float Gamma);
+
+		[DllImport(dllName, EntryPoint = "iluGenImage")]
+		public static extern int GenImage(); // Deprecated
+
+		[DllImport(dllName, EntryPoint = "iluGetImageInfo")]
+		public static extern void GetImageInfo(ref Info Info);
+
+		[DllImport(dllName, EntryPoint = "iluGetInteger")]
+		public static extern int GetInteger(IntName Mode);
+
+		[DllImport(dllName, EntryPoint = "iluGetIntegerv")]
+		public static extern void GetIntegerv(IntName Mode, int[] Param);
+
+		[DllImport(dllName, EntryPoint = "iluGetString")]
+		public static extern string GetString(StringName StringName);
+
+		[DllImport(dllName, EntryPoint = "iluImageParameter")]
+		public static extern void ImageParameter(ImageParameterName PName, int Param);
+
+		[DllImport(dllName, EntryPoint = "iluInit")]
+		public static extern void Init();
+
+		[DllImport(dllName, EntryPoint = "iluInvertAlpha")]
+		public static extern bool InvertAlpha();
+
+		[DllImport(dllName, EntryPoint = "iluLoadImage")]
+		public static extern int LoadImage(string FileName);
+
+		[DllImport(dllName, EntryPoint = "iluMirror")]
+		public static extern bool Mirror();
+
+		[DllImport(dllName, EntryPoint = "iluNegative")]
+		public static extern bool Negative();
+
+		[DllImport(dllName, EntryPoint = "iluNoisify")]
+		public static extern bool Noisify(float Tolerance);
+
+		[DllImport(dllName, EntryPoint = "iluPixelize")]
+		public static extern bool Pixelize(int PixSize);
+
+		[DllImport(dllName, EntryPoint = "iluRegionfv")]
+		public static extern void Regionfv(P2f[] Points, int n);
+
+		[DllImport(dllName, EntryPoint = "iluRegioniv")]
+		public static extern void Regioniv(P2i[] Points, int n);
+
+		[DllImport(dllName, EntryPoint = "iluReplaceColour")]
+		public static extern bool ReplaceColour(byte Red, byte Green, byte Blue, float Tolerance);
+
+		[DllImport(dllName, EntryPoint = "iluRotate")]
+		public static extern bool Rotate(float Angle);
+
+		[DllImport(dllName, EntryPoint = "iluRotate3D")]
+		public static extern bool Rotate3D(float x, float y, float z, float Angle);
+
+		[DllImport(dllName, EntryPoint = "iluSaturate1f")]
+		public static extern bool Saturate1f(float Saturation);
+
+		[DllImport(dllName, EntryPoint = "iluSaturate4f")]
+		public static extern bool Saturate4f(float r, float g, float b, float Saturation);
+
+		[DllImport(dllName, EntryPoint = "iluScale")]
+		public static extern bool Scale(int Width, int Height, int Depth);
+
+		[DllImport(dllName, EntryPoint = "iluScaleAlpha")]
+		public static extern bool ScaleAlpha(float scale);
+
+		[DllImport(dllName, EntryPoint = "iluScaleColours")]
+		public static extern bool ScaleColours(float r, float g, float b);
+
+		[DllImport(dllName, EntryPoint = "iluSepia")]
+		public static extern bool Sepia();
+
+		[DllImport(dllName, EntryPoint = "iluSetLanguage")]
+		public static extern bool SetLanguage(Language Language);
+
+		[DllImport(dllName, EntryPoint = "iluSharpen")]
+		public static extern bool Sharpen(float Factor, int Iter);
+
+		[DllImport(dllName, EntryPoint = "iluSwapColours")]
+		public static extern bool SwapColours();
+
+		[DllImport(dllName, EntryPoint = "iluWave")]
+		public static extern bool Wave(float Angle);
+	}
+}
